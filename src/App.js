8423fde6c40e61bfcdb75588e2387733d9641d1e
yoyo <timotius_albert@yahoo.com>
2022-01-28 15:47:48 +0700

    ubersnap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useCallback,useEffect} from "react";
+import "./App.css"; 
+import axios from "axios";
+import { Component } from "react/cjs/react.production.min";
 
-function App() {
+function App(){
+  const [data,setData] = useState([]);
+  const [fName,setFname] = useState([]);
+  const [lName,setLname] = useState([]);
+
+  function click(){
+  axios.get("https://reqres.in/api/users").then(function(res){
+    setData(res.data.data);
+    console.log(res);
+  })
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={click}>
+        Click
+      </button>
+      <h1>ID</h1>
+      <p>{data.map(function(data){
+        return data.id;
+      })}</p>
+      <h1>First name</h1>
+      <p>
+      {data.map(function(data){
+        return data.first_name + ", ";
+      })}
+      </p>
+      <h1>Last name</h1>
+      <p>
+        {data.map(function(data){
+        return data.last_name + ", ";
+      })
+
+        }
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;
